76ac3decac9f32868dfa65708315d4f458ede3f4
JIMMY GUO <57852430+JimmyKuo-24@users.noreply.github.com>
2024-08-19 17:10:06 +0900

    Update line_bot.py

--- line_bot.py
+++ line_bot.py
@@ -3,5 +3,5 @@ from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (CarouselColumn, CarouselTemplate, ImageSendMessage, MessageAction, MessageEvent, TemplateSendMessage, TextMessage,
                             TextSendMessage, StickerSendMessage, URIAction)
 
-line_bot_api = LineBotApi(${{ secrets.LINE_ACCESS_TOKEN }})
-handler = WebhookHandler(${{ secrets.LINE_CHANNEL_SECRET }})
+line_bot_api = LineBotApi('${{ secrets.LINE_ACCESS_TOKEN }}')
+handler = WebhookHandler('${{ secrets.LINE_CHANNEL_SECRET }}')
